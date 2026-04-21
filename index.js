022e16a295e2077230d2467a0676bee1319bdf93
Alexandre Carvalho <ale.caarvalho@gmail.com>
2026-04-21 15:43:48 -0300

    mensagem da atualizaçã2

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.post('/webhook', async (req,res)=>{
-  const msg = req.body?.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
+app.post('/webhook', async (req, res) => {
+  console.log("📩 mensagem recebida:", JSON.stringify(req.body, null, 2));
+
+  const msg = req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
+
   if(msg){
     await handleMessage(msg);
   }
+
   res.sendStatus(200);
 });
 
