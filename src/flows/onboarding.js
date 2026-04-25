390466549ac66f0e22ff5b2b1ec48333515e68b8
Alexandre Carvalho <ale.caarvalho@gmail.com>
2026-04-25 15:33:43 -0300

    debug webhook response74

--- src/flows/onboarding.js
+++ src/flows/onboarding.js
@@ -219,7 +219,7 @@ export async function handleOnboarding({
 
     return sendText(
       phone,
-      "Qual sua cidade?\n\nVocê pode escrever só a cidade ou cidade + estado.\nExemplos:\n• Itabaiana - SE"
+      "Qual sua cidade?\n\nVocê pode escrever cidade + estado.\nExemplo:\n• Itabaiana - SE"
     );
   }
 
@@ -262,7 +262,7 @@ export async function handleOnboarding({
 
       return sendText(
         phone,
-        "Qual sua cidade?\n\nVocê pode escrever só a cidade ou cidade + estado.\nExemplos:\n\• Itabaiana - SE"
+        "Qual sua cidade?\n\nVocê pode escrever cidade + estado.\nExemplo:\n\• Itabaiana - SE"
       );
     }
 
